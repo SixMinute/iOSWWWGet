d1140aa83b046b7fa2adf9acb09667ed27d2cf97
seaders <seaders69@gmail.com>
2015-06-12 10:17:48 +0100

    Initial commit.

---
+++ Assets/MainScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class MainScript : MonoBehaviour
+{
+	public string _URL = null;
+
+	public void OnGUI()
+	{
+		if (GUI.Button(new Rect(15, 15, 300, 100), "WWWGet"))
+		{
+			WWWGet();
+		}
+	}
+	
+	public void OnApplicationPause(bool isSuspended)
+	{
+		if(isSuspended)
+		{
+			WWWGet();
+		}
+	}
+
+	void WWWGet()
+	{
+		WWW www = new WWW(_URL);
+		while(!www.isDone)
+		{
+			Thread.Sleep(100);
+		}
+		Debug.Log(www.text);
+	}
+}
